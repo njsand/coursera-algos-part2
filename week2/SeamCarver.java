2fba5af7b6401a1b9bd45652c2f3e9e9fa81525b
Nick Sandow <nick.sandow@gmail.com>
2023-08-19 17:22:21 +1000

    Starting week2

---
+++ week2/SeamCarver.java
@@ -0,0 +1,57 @@
+import edu.princeton.cs.algs4.Picture;
+
+public class SeamCarver {
+
+    private final Picture picture;
+
+   // create a seam carver object based on the given picture
+    public SeamCarver(Picture picture) {
+        this.picture = picture;
+    }
+
+   // current picture
+    public Picture picture() {
+        return null;
+    }
+
+   // width of current picture
+    public int width() {
+        return 0;
+    }
+
+   // height of current picture
+    public int height() {
+        return 0;
+    }
+
+   // energy of pixel at column x and row y
+    public double energy(int x, int y) {
+        return 0;
+    }
+
+   // sequence of indices for horizontal seam
+    public int[] findHorizontalSeam() {
+        return new int[]{};
+    }
+
+   // sequence of indices for vertical seam
+    public int[] findVerticalSeam() {
+        return new int[]{};        
+    }
+
+   // remove horizontal seam from current picture
+    public void removeHorizontalSeam(int[] seam) {
+        
+    }
+
+   // remove vertical seam from current picture
+    public void removeVerticalSeam(int[] seam) {
+        
+    }
+
+   //  unit testing (optional)
+    public static void main(String[] args) {
+        
+    }
+
+}
