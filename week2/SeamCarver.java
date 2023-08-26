650959ee0e360c8766eceaa4c8616e0b4fb71416
Nick Sandow <nick.sandow@gmail.com>
2023-08-26 16:44:59 +1000

    Add findVerticalSeam()

--- week2/SeamCarver.java
+++ week2/SeamCarver.java
@@ -1,3 +1,5 @@
+// Part of https://coursera.cs.princeton.edu/algs4/assignments/seam/specification.php
+
 import java.awt.Color;
 
 import edu.princeton.cs.algs4.Picture;
@@ -6,27 +8,37 @@ public class SeamCarver {
 
     private final Picture picture;
 
-   // create a seam carver object based on the given picture
+    private final double[][] energies;
+
+    // create a seam carver object based on the given picture
     public SeamCarver(Picture picture) {
         this.picture = picture;
+
+        energies = new double[height()][width()];
+        
+        for (int i = 0; i < height(); i++) {
+            for (int j = 0; j < width(); j++) {
+                energies[i][j] = energy(j, i);
+            }
+        }
     }
 
-   // current picture
+    // current picture
     public Picture picture() {
-        return null;
+        return picture;
     }
 
-   // width of current picture
+    // width of current picture
     public int width() {
-        return 0;
+        return picture.width();
     }
 
-   // height of current picture
+    // height of current picture
     public int height() {
-        return 0;
+        return picture.height();
     }
 
-   // energy of pixel at column x and row y
+    // energy of pixel at column x and row y
     public double energy(int x, int y) {
         if (isEdge(x, y))
             return 1000;
@@ -34,20 +46,23 @@ public class SeamCarver {
         Color left = picture.get(x-1, y);
         Color right = picture.get(x+1, y);
         
-        double xrGradient = (right.getRed() - left.getRed()) ^ 2;
-        double xgGradient = (right.getGreen() - left.getGreen()) ^ 2;
-        double xbGradient = (right.getBlue() - left.getBlue()) ^ 2;
+        double xrDiff = right.getRed() - left.getRed();
+        double xgDiff = right.getGreen() - left.getGreen();
+        double xbDiff = right.getBlue() - left.getBlue();
 
         Color above = picture.get(x, y-1);
         Color below = picture.get(x, y+1);
         
-        double yrGradient = (below.getRed() - above.getRed()) ^ 2;
-        double ygGradient = (below.getGreen() - above.getGreen()) ^ 2;
-        double ybGradient = (below.getBlue() - above.getBlue()) ^ 2;
+        double yrDiff = below.getRed() - above.getRed();
+        double ygDiff = below.getGreen() - above.getGreen();
+        double ybDiff = below.getBlue() - above.getBlue();
 
-        return Math.pow(xrGradient + xgGradient + xbGradient +
-                        yrGradient + ygGradient + ybGradient,
-                        0.5);
+        return Math.sqrt(xrDiff * xrDiff +
+                         xgDiff * xgDiff +
+                         xbDiff * xbDiff +
+                         yrDiff * yrDiff +
+                         ygDiff * ygDiff +
+                         ybDiff * ybDiff);
     }
 
     private boolean isEdge(int x, int y) {
@@ -60,27 +75,88 @@ public class SeamCarver {
         return false;
     }
     
-   // sequence of indices for horizontal seam
+    // sequence of indices for horizontal seam
     public int[] findHorizontalSeam() {
         return new int[]{};
     }
 
-   // sequence of indices for vertical seam
+    // sequence of indices for vertical seam
     public int[] findVerticalSeam() {
-        return new int[]{};        
+        int pathTo[][] = new int[height()][width()];
+        double distTo[] = new double[width()];
+        double prevDistTo[] = new double[width()];
+
+        // Init prevDistTo to the first row's energies.
+        for (int i = 0; i < width(); i++)
+            prevDistTo[i] = energies[0][i];
+
+        // Starting from the second row, for each pixel, choose the above
+        // (connected) pixel with the shortest path.
+        for (int i = 1; i < height(); i++) {
+            for (int j = 0; j < width(); j++) {
+                int prev = choosePath(prevDistTo, i, j);
+
+                distTo[j] = prevDistTo[prev] + energies[i][j];
+                pathTo[i][j] = prev;
+            }
+
+            // Swap.
+            double[] tmp = distTo;
+            distTo = prevDistTo;
+            prevDistTo = tmp;
+            
+        }
+
+        // Find the end of the path.  It's
+        int pos = findMin(distTo);
+
+        int[] path = new int[height()];
+
+        path[path.length-1] = pos;
+
+        // Now construct the path, starting from the bottom.
+        for (int i = height()-2; i >=0; i--) {
+            path[i] = pos = pathTo[i+1][pos];
+        }
+        
+        return path;
     }
 
-   // remove horizontal seam from current picture
+    int findMin(double []a) {
+        double min = a[0];
+        int minPos = 0;
+        
+        for (int i = 1; i < a.length; i++)
+            if (a[i] < min) {
+                min = a[i];
+                minPos = i;
+            }
+
+        return minPos;
+    }
+
+    int choosePath(double[] prevDistTo, int i, int j) {
+        int aboveLeft = j == 0 ? j : j-1;
+        int above = j;
+        int aboveRight = j == width()-1 ? j : j+1;
+
+        if (prevDistTo[aboveLeft] < prevDistTo[above])
+            return prevDistTo[aboveLeft] < prevDistTo[aboveRight] ? aboveLeft : aboveRight;
+        else
+            return prevDistTo[above] < prevDistTo[aboveRight] ? above : aboveRight;
+    }
+    
+    // remove horizontal seam from current picture
     public void removeHorizontalSeam(int[] seam) {
         
     }
 
-   // remove vertical seam from current picture
+    // remove vertical seam from current picture
     public void removeVerticalSeam(int[] seam) {
         
     }
 
-   //  unit testing (optional)
+    //  unit testing (optional)
     public static void main(String[] args) {
         
     }
